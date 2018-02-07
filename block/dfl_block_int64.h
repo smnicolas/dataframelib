39b49b2f9a48594b62e02768f5733c892665a2c9
smnicolas <smnicolas@gmail.com>
2018-02-07 12:50:30 -0300

    Primer commit
    La idea es usar block para manejar la memoria, series para acceder

---
+++ block/dfl_block_int64.h
@@ -0,0 +1,20 @@
+#ifndef __DFL_BLOCK_INT64_H__
+#define __DFL_BLOCK_INT64_H__
+
+#include <stdint.h>
+#include <stddef.h>
+
+struct dfl_block_int64_struct {
+     size_t size;
+     int64_t *data;
+}
+
+#endif
+
+
+
+
+
+
+
+
---
+++ series/dfl_series_int64.h
@@ -0,0 +1,11 @@
+#ifndef __DFL_SERIES_INT64_H__
+#define __DFL_SERIES_INT64_H__
+
+#include <dfl/dfl_block_int64.h>
+
+typedef struct {
+     size_t size;
+     dfl_block_int64 *block;
+     int owner;
+}
+#endif
