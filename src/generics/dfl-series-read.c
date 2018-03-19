2916b9f3ade029fdb2379a97404bd9aaab257a7d
nsm <smnicolas>
2018-03-18 22:20:55 -0300

    Compila linux

--- src/generics/dfl-series-read.c
+++ src/generics/dfl-series-read.c
@@ -1,10 +1,15 @@
-#include "<dfl/dfl_generics.h>"
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+
+#include <dfl/dfl_generics.h>
+#include <dfl/dfl_series.h>
 
 static void dfl_series_read_func_array_constructor ()
      __attribute__((constructor));
 
 
-int
+dfl_series*
 not_implemented_func_series_btype_FILE (
      dfl_basic_type btype, FILE * f) {
      fprintf(stderr,"function not implemented\n");
@@ -12,35 +17,35 @@ not_implemented_func_series_btype_FILE (
      return 0;
 }
 
-dfl_series * dfl_series_read_int(char *string) { 
+/* dfl_series * dfl_series_read_int(char *string) {  */
 
-     int sum = 0;
-     dfl_series_read_int * series;
-     dfl_series_alloc(series);
-     while (1) {
-          char *tail;
-          int next;
+/*      int sum = 0; */
+/*      dfl_series * series; */
+/*      //dfl_series_alloc(series); */
+/*      while (1) { */
+/*           char *tail; */
+/*           int next; */
 
-          while (isspace (*string)) string++;
-          if (*string == 0)
-               break;
+/*           while (isspace (*string)) string++; */
+/*           if (*string == 0) */
+/*                break; */
 
-          errno = 0;
-          next = strtol (string, &tail, 0);
-          if (errno)
-               printf ("Overflow\n");
-          else
-               sum += next;
-          string = tail;
-     }
+/*           errno = 0; */
+/*           next = strtol (string, &tail, 0); */
+/*           if (errno) */
+/*                printf ("Overflow\n"); */
+/*           else */
+/*                sum += next; */
+/*           string = tail; */
+/*      } */
 
-          return sum;
-     }
+/*           return sum; */
+/* } */
 
      
-}
 
-dfl_int_func_void_const_void_const
+
+dfl_series_func_btype_FILE
 dfl_series_read_func_array[dfl_basic_types_MAX + 1];
 
 void dfl_series_read_func_array_constructor () {
@@ -55,6 +60,6 @@ void dfl_series_read_func_array_constructor () {
 }
 
 
-dfl_series *dfl_series_read(dfl_basic_type btype, FILE * f) {
-     return dfl_series_read_func_array[type](a, b);     
-}
+/* dfl_series *dfl_series_read(dfl_basic_type btype, FILE * f) { */
+/*      return dfl_series_read_func_array[btype](a, b);      */
+/* } */
--- src/generics/dfl_generics.h
+++ src/generics/dfl_generics.h
@@ -2,7 +2,7 @@
 #define __DFL_GENERICS_H__
 
 #include <dfl/dfl_types.h>
-
+#include <dfl/dfl_series.h>
 
 int dfl_cmp (dfl_basic_type type, void const *a, void const *b);
 
@@ -10,7 +10,7 @@ typedef int (*dfl_int_func_void_const_void_const)
 (void const *, void const *);
 
 typedef dfl_series* (*dfl_series_func_btype_FILE)
-(void const *, void const *);
+     (dfl_basic_type btype, FILE * f);
 
 extern dfl_int_func_void_const_void_const
 dfl_cmp_func_array[dfl_basic_types_MAX + 1];
